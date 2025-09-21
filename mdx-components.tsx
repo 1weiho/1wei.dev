336410a7cbfb35dc6dc24d29d932e460d94de676
Yiwei Ho <davidho0403@gmail.com>
2025-09-21 23:41:39 +0800

    chore: improve blog content page design

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -5,8 +5,11 @@ import { unstable_ViewTransition as ViewTransition } from 'react'
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     wrapper: ({ children }) => (
-      <article className="prose mx-auto my-20 md:my-28">{children}</article>
+      <article className="prose mx-auto my-20 md:my-28 prose-headings:font-[family-name:var(--font-instrument-serif)] prose-headings:text-4xl prose-headings:font-normal prose-p:my-6 md:prose-p:my-8">
+        {children}
+      </article>
     ),
+    a: (props) => <a target="_blank" rel="noopener noreferrer" {...props} />,
     ...components,
   }
 }
