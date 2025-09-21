291586324d2619257c278967e26f3b0c12b9634a
Yiwei Ho <davidho0403@gmail.com>
2025-09-21 23:49:16 +0800

    feat: blog metadata and og image font improve

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,6 +1,21 @@
 import { ImageResponse } from 'next/og'
 import { NextRequest } from 'next/server'
 
+async function loadGoogleFont(font: string, text: string) {
+  const url = `https://fonts.googleapis.com/css2?family=${font}&text=${encodeURIComponent(text)}`
+  const css = await (await fetch(url)).text()
+  const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/)
+
+  if (resource) {
+    const response = await fetch(resource[1])
+    if (response.status == 200) {
+      return await response.arrayBuffer()
+    }
+  }
+
+  throw new Error('failed to load font data')
+}
+
 export async function GET(request: NextRequest) {
   const { searchParams } = request.nextUrl
   const title = searchParams.get('title')
@@ -30,6 +45,7 @@ export async function GET(request: NextRequest) {
             color: '#4F4F4F',
             lineHeight: '110px',
             whiteSpace: 'pre-wrap',
+            fontFamily: 'Instrument Serif',
           }}
         >
           {title}
@@ -39,6 +55,13 @@ export async function GET(request: NextRequest) {
     {
       width: 1200,
       height: 630,
+      fonts: [
+        {
+          name: 'Instrument Serif',
+          data: await loadGoogleFont('Instrument Serif', title ?? ''),
+          style: 'normal',
+        },
+      ],
     },
   )
 }
